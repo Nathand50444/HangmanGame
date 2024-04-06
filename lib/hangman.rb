bf60aeef655be23d69687ef2e925a2899911172c
NathanD <nathand50444@gmail.com>
2024-04-06 20:03:01 +0100

    initial commit
    doc created with instructions listed for project

---
+++ lib/hangman.rb
@@ -0,0 +1,26 @@
+class hangman
+
+    # 1. When a new game is started, your script should load in the dictionary and randomly select...
+    # a word between 5 and 12 characters long for the secret word.
+
+    # 2. Draw an stick figure as the game goes on and display a count so the player knows how many more... 
+    # incorrect guesses they have before the game ends. 
+    
+    # 3. You should display which CORRECT letters have already been chosen...
+    # (and their position in the word, e.g. _ r o g r a _ _ i n g) and which INCORRECT letters have already been chosen.
+
+    # 4. Every turn, allow the player to make a case insensitive guess of a letter. Update the display for correct or incorrect. 
+    # If out of guesses, the player should lose.
+
+    # 5. Now implement the functionality where, at the start of any turn, instead of making a guess 
+    # the player should also have the option to save the game. 
+    # Remember what you learned about serializing objects… you can serialize your game class too!
+
+    # 6. When the program first loads, add in an option that allows you to open one of your saved games... 
+    # which should jump you exactly back to where you were when you saved. Play on!
+    
+    def initialize
+    
+    end
+
+end
